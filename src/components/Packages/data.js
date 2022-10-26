37297450ad587172cacf0adfcefb2ab54e1b1870
mdsajalali <itzsajal.me@gmail.com>
2022-10-26 20:51:43 +0600

    data-mapping

---
+++ src/components/Packages/data.js
@@ -0,0 +1,70 @@
+const cardsData = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/QKmBvkx/kuakata.jpg",
+      title: "MEDIUM 5-DAY TOUR",
+      text: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      place: "Patuakhali",
+      people: "20 people",
+      perPerson: "$550 per person",
+      book: "Book now",
+    },
+  
+    {
+      id: 2,
+      img: "https://i.ibb.co/Ybd2P4M/sundarban.jpg",
+      title: "DIFFICULT 7-DAY TOUR",
+      text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      place: "Cox's bazar",
+      people: "25 people",
+      perPerson: "$700 per person",
+      book: "Book now",
+    },
+  
+    {
+      id: 3,
+      img: "https://i.ibb.co/KLXrNHR/jaflong.jpg",
+      title: "MEDIUM 5-DAY TOUR",
+      text: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      place: "Kuakatai",
+      people: "15 people",
+      perPerson: "$500 per person",
+      book: "Book now",
+    },
+  
+    {
+      id: 4,
+      img: "https://i.ibb.co/27Z99CM/Sajek.png",
+      title: "MEDIUM 5-DAY TOUR",
+      text: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      place: "Sajek",
+      people: "20 people",
+      perPerson: "$750 per person",
+      book: "Book now",
+    },
+  
+    {
+      id: 5,
+      img: "https://i.ibb.co/z2fvw0Z/Sreemongol.png",
+      title: "DIFFICULT 7-DAY TOUR",
+      text: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      place: "Sreemongo",
+      people: "25 people",
+      perPerson: "$800 per person",
+      book: "Book now",
+    },
+  
+    {
+      id: 6,
+      img: "https://i.ibb.co/0KQqm7g/sundorbon.png",
+      title: "MEDIUM 5-DAY TOUR",
+      text: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      place: "Sundorbon",
+      people: "15 people",
+      perPerson: "$900 per person",
+      book: "Book now",
+    },
+  ];
+  
+  export default cardsData;
+  
--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HelmetProvider>
-    <BrowserRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </BrowserRouter>
-  </HelmetProvider>
 );
